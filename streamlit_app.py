a277c9ac468d493ddb590edb3c5e569771cb21d8
Williams <agbanagbawilliams@gmail.com>
2024-08-27 08:03:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ import time
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #DC0901;">
-  <a class="navbar-brand" target="_blank">Multi app</a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #F0DEAB;">
+  <a class="navbar-brand" target="_blank">grende</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/">About me <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://newwebpage-nr9d95duhpvvbdx6mqjedc.streamlit.app/Project" target="_blank">Project</a>
